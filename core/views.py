65e372988dd373341512a4ccef06eb3936eeb32c
LucasQuintela <lucasquintela.dev@gmail.com>
2021-08-11 00:08:44 -0300

    finalizando o projeto

--- core/views.py
+++ core/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
+from .models import Produto
 
 
 def index(request):
+    print(dir(request))
+    print(request)
+    produtos = Produto.objects.all()
     context = {
         'curso': 'programação web',
-        'outro': 'Django e Massa'
+        'outro': 'Django e Massa',
+        'produtos': produtos
     }
     return render(request, 'index.html', context)
 
 def contato(request):
     return render(request, 'contato.html')
+
+def produto(request, pk):
+    prod = Produto.objects.get(id=pk)
+    context = {
+        'produto': prod
+    }
+    return render(request, 'produto.html', context)
--- projeto3/settings.py
+++ projeto3/settings.py
@@ -120,4 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' # usado durante o desenvolvimento
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #usado durante a produção
+
+
+
